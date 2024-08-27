3adf8fb46f81fbffa8abb81be6fce11e573e8d9d
sebbur04 <sebaseb@stud.ntnu.no>
2024-08-27 18:41:53 +0200

    Endringer for at C Koden skal kjøre

--- OBL1_3_OS_IDATT2202.c
+++ OBL1_3_OS_IDATT2202.c
@@ -1,15 +1,17 @@
-#include <stdio.h> #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 void func() {
-char b = 'b';
-/*long localvar = 2;
-printf("func() with localvar @ 0x%08x\n", &localvar);
-printf("func() frame address @ 0x%08x\n", __builtin_frame_address(0));
-Page 2
-localvar++;*/
-b = 'a';
-func(); }
+    char b = 'b';
+    /*long localvar = 2;
+    printf("func() with localvar @ %p\n", (void*)&localvar);
+    printf("func() frame address @ %p\n", __builtin_frame_address(0));
+    localvar++;*/
+    b = 'a';
+}
+
 int main() {
-    printf("main() frame address @ 0x%08x\n", __builtin_frame_address(0));
+    printf("main() frame address @ %p\n", __builtin_frame_address(0));
     func();
     exit(0);
 }
